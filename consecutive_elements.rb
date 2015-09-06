5d27ddbc154579c8a895f99bf9edd45602173436
Marvin Laucher <marv070@gmail.com>
2015-09-06 18:40:01 -0400

    tested to see an array with elements

--- consecutive_elements.rb
+++ consecutive_elements.rb
@@ -1,4 +1,4 @@
 def fill_my_array
-our_array = []
+our_array = [*(1..10)]
 end
 fill_my_array
--- consecutive_elements_test.rb
+++ consecutive_elements_test.rb
@@ -1,4 +1,4 @@
-require_relative"consecutive_elements_kata.rb"
+require_relative"consecutive_elements.rb"
 require "minitest/autorun"
 
 class Consecutive_elements_test < Minitest::Test
@@ -7,6 +7,12 @@ def test_to_see_if_there_is_an_array
   results = fill_my_array
   assert_equal(Array, results.class)
 end
+
+def test_for_elements_in_array
+  results = fill_my_array
+  assert_equal(10, results.count)
+end
+
 
 
 
